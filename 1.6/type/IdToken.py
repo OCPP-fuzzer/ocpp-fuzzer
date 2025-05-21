9840a5f57b5f0f0321374ec2e4f29619c223f488
mjshin <mjshin@enki.co.kr>
2025-05-21 18:02:04 +0900

    update: modified IdToken type

--- 1.6/type/IdToken.py
+++ 1.6/type/IdToken.py
@@ -1,23 +1,10 @@
-from enum import Enum
+from type.CiString20Type import CiString20Type
     
-class IdToken(Enum):
-
-    """
-    EnumType class
-    : CiString20Type : IdToken is case insensitive.
-    """
-
-    CISTRING20TYPE = "CiString20Type"
-
+class IdToken(CiString20Type):
     def get_value(self):
-        return self.value
+        return self
 
     @classmethod
     def get_members(cls):
         return [m for m in cls]
-
-    @classmethod
-    def get_sample(cls, option=False):
-        value = __import__('random').choice(list(cls))
-        return value.value
     
